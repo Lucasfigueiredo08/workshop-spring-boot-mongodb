38ff75b1544a0acd01056b25486690db58835a1d
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-06-08 17:13:40 -0300

    referenciando os posts do usuario

--- src/main/java/com/lucasliberato/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucasliberato/workshopmongo/config/Instantiation.java
@@ -42,9 +42,12 @@ public class Instantiation implements CommandLineRunner {
         var post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Joinville, Abraços", new AuthorDTO(maria));
         var post2 = new Post(null, sdf.parse("08/06/2024"), "Bom dia", "O sou já nasceu lá na fazendinha!", new AuthorDTO(maria));
 
-
-
-
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+
+        userRepository.save(maria);
+
+
     }
 }
